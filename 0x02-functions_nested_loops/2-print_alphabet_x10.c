59f5e5b0b139a8cc3da0edb470b36051bbb2f6ed
oussama <oussama0habiballah5@gmail.com>
2023-03-21 06:11:27 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,16 @@
  * Return: viod
  */
 void print_alphabet_x10(void);
-int i = 0, j = 97;
+int i = 0;
 while (i < 10)
 {
-while (j <= 122)
+char c = 'a';
+while (c <= 'z')
 {
-_putchar(j);
-j++;
+_putchar(c);
+c++;
 }
 _putchar('\n');
-j = 97;
 i++;
 }
 }
