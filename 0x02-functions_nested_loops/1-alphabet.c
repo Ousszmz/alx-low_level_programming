431975a4d45abbb79e799b084e4a8e93bb32d2cc
oussama <oussama0habiballah5@gmail.com>
2023-03-21 05:14:45 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print_alphabet - lowercase alphabets
+ * Return: void
+ */
+int main(void)
+{
+_putchat('abcdefghijklmnopqrstuvwxyz');
+return (0);
+}
