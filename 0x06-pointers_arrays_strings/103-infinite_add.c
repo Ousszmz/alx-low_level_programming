5bf476e532ecbe605fa4bbc10794189c26f33b82
oussama <oussama0habiballah5@gmail.com>
2023-03-30 18:16:49 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,93 +1,78 @@
 #include "main.h"
 
-int _strlen(const char *str);
-int getcharval(const char *s, int idx);
-void reverse_str(char *str, int len);
-
 /**
- * infinite_add - add 2 numbers together from two strings
- * @n1: 1st string
- * @n2: 2nd string
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: total
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int i, w_n1 = _strlen(n1), w_n2 = _strlen(n2), sum, carry;
 
-	if (w_n1 >= size_r || w_n2 >= size_r)
-		return (0);
-
-	for (i = 0; i < w_n1 || i < w_n2; i++)
-	{
-		char cn1 = getcharval(n1, i);
-		char cn2 = getcharval(n2, i);
-
-		sum = cn1 + cn2 + carry;
-		carry = 0;
-		if (sum > 9)
-		{
-			carry = 1;
-			sum -= 10;
-		}
-		r[i] = sum + 48;
-	}
-
-	if (carry)
-		r[i++] = carry + 48;
-	if (i >= size_r)
-		return (0);
-
-	r[i] = '\0';
-
-	reverse_str(r, _strlen(r));
-
-	return (r);
-}
-
-/**
- * _strlen - Self explain
- * @str: the string
- * Return: (int) the length
- **/
-int _strlen(const char *str)
-{
-	int len = 0;
-
-	while (str[len])
-		len++;
-
-	return (len);
-}
-
-/**
- * getcharval - Self explain
- * @s: the string
- * @idx: index of char in string
- * Return: (int) value of char in ascii
- **/
-int getcharval(const char *s, int idx)
-{
-	if (idx < _strlen(s))
-		return (s[_strlen(s) - idx - 1] - 48);
-	return (0);
-}
-
-/**
- * reverse_str - Self explain
- * @str: the string
- * @len: lenght of the given string
- **/
-void reverse_str(char *str, int len)
+void rev_string(char *n)
 {
 	int i = 0;
+	int j = 0;
+	char temp;
 
-	for (i = 0; i < len / 2; i++)
+	while (*(n + i) != '\0')
 	{
-		char t = str[i];
-
-		str[i] = str[len - i - 1];
-		str[len - i - 1] = t;
+		i++;
 	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
