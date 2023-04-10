e5c20914d7e3953e39f15138863e8b468da0f397
oussama <oussama0habiballah5@gmail.com>
2023-04-10 15:24:40 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - checking odf positive numbers
+ * @argc: number of args
+ * @argv: an array of args
+ * Return: 0 if success otherwise 1
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0, i, j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
